3d7cbaedaa7c43693d03252f6973b68027869dbc
Craig Storer <craigstorer@gmail.com>
2019-12-18 23:27:58 +0000

    Done.

---
+++ include.rb
@@ -0,0 +1,4 @@
+the_numbers = [4,8,15,16,23,42]
+the_numbers.include?(42)   #=> true
+the_numbers.include?(6)  
+
---
+++ none.rb
@@ -0,0 +1,7 @@
+none_even = true
+[1,3].each do |i|
+  if i.even?
+    none_even = false
+  end
+end #=> [1,3] because `#each` always returns the original collection
+none_even #=> true
